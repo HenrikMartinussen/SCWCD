c30122d19df7d13582548212c2d93af0cf30a2dc
henmar <henrik.martinussen@gmail.com>
2010-10-28 02:04:02 +0200

    as is...

---
+++ ch4/src/test/java/info/martinussen/scwcd/ch4/model/TestBeerExpert.java
@@ -0,0 +1,52 @@
+package info.martinussen.scwcd.ch4.model;
+
+import info.martinussen.scwcd.ch4.model.BeerExpert;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class TestBeerExpert extends TestCase {
+  private BeerExpert testExpert = null;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  protected void tearDown() throws Exception {
+    testExpert = null;
+    super.tearDown();
+  }
+
+  public void testGetBrandsForAmber() {
+    testExpert = new BeerExpert();
+    List<String> result = testExpert.getBrands("amber");
+    assertEquals(2, result.size());
+    assertTrue(result.contains("Jack Amber"));
+    assertTrue(result.contains("Red Moose"));
+  }
+
+  public void testGetBrandsForDark() {
+    testExpert = new BeerExpert();
+    List<String> result = testExpert.getBrands("dark");
+    assertEquals(2, result.size());
+    assertTrue(result.contains("Jail Pale Ale"));
+    assertTrue(result.contains("Gout Stout"));
+  }
+
+  public void testGetBrandsForLight() {
+    testExpert = new BeerExpert();
+    List<String> result = testExpert.getBrands("light");
+    assertEquals(2, result.size());
+    assertTrue(result.contains("Jail Pale Ale"));
+    assertTrue(result.contains("Gout Stout"));
+  }
+  
+  public void testGetBrandsForBrown() {
+    testExpert = new BeerExpert();
+    List<String> result = testExpert.getBrands("brown");
+    assertEquals(2, result.size());
+    assertTrue(result.contains("Jail Pale Ale"));
+    assertTrue(result.contains("Gout Stout"));
+  }
+}
