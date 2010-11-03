a5b8cd19d82a0b4b101d0a6188ab4680d2c62568
henmar <hma@mail.dk>
2010-11-03 18:28:11 +0100

    ch1 example complete

--- ch1/src/main/java/info/martinussen/web/Ch1Servlet.java
+++ ch1/src/main/java/info/martinussen/web/Ch1Servlet.java
@@ -4,13 +4,17 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class SimpleServlet extends HttpServlet {
+public class Ch1Servlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-        out.println( "SimpleServlet Executed" );
+	java.util.Date today = new java.util.Date();
+        out.println("<html>" + 
+                    "<body>" + 
+                    "<h1 align=\'center\'>HF\'s Chapter1 Servlet</h1>" +
+                    "<br/>" + today + "</body>" + "</html>");
         out.flush();
         out.close();
     }
