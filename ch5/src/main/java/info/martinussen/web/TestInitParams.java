c741659c7e0e6296c5094bad66e6fbc8a7489b65
henmar <henrik.martinussen@gmail.com>
2010-11-05 01:57:33 +0100

    getInitParameters

--- ch5/src/main/java/info/martinussen/web/TestInitParams.java
+++ ch5/src/main/java/info/martinussen/web/TestInitParams.java
@@ -1,6 +1,8 @@
 package info.martinussen.web;
 
 import java.io.*;
+import java.util.Enumeration;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -13,7 +15,15 @@ public class TestInitParams extends HttpServlet {
     	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         
-        out.println( "Test init parameters<br/>" );
+        out.println("Test init parameters<br/>" );
+        
+        Enumeration e = getServletConfig().getInitParameterNames();
+        while (e.hasMoreElements()){
+        	out.println("<br/>" + "param name = " + e.nextElement() + "<br/>");
+        }
+        out.println("Main Email is: " + getServletConfig().getInitParameter("adminEmail"));
+        out.println("<br/>");
+        out.println("admin Email is: " + getServletConfig().getInitParameter("mainEmail"));
         out.flush();
         out.close();
     }
