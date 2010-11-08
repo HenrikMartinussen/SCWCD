9d485ae73468b158db9d1418d24a90e1cd6ec281
henmar <henrik.martinussen@gmail.com>
2010-11-08 14:31:20 +0100

    added additional life cycle console trace messages to ListenerTester and MyServletContextListener

--- ch5-context-listener/src/main/java/info/martinussen/scwcd/hfsj/ch5/contexttest/ListenerTester.java
+++ ch5-context-listener/src/main/java/info/martinussen/scwcd/hfsj/ch5/contexttest/ListenerTester.java
@@ -1,23 +1,56 @@
 package info.martinussen.scwcd.hfsj.ch5.contexttest;
 
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+@SuppressWarnings("serial")
 public class ListenerTester extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  
+  static {
+    System.out.println("ListenerTester is being loaded");
+  }
+  
+  public ListenerTester(){
+    super();
+    System.out.println("ListenerTester is being constructed");
+  }
+  
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    System.out.println("ListenerTester is being initialized");
+  }
+  
+  
+  
+  @Override
+  public void destroy() {
+    super.destroy();
+    System.out.println("ListenerTester is being destroyed");
+  }
 
-    	response.setContentType("text/html");
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("ListenerTester.service(req, resp) runs");
+    super.service(req, resp);
+  }
+
+
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("ListenerTester.doGet(req, resp) runs");
+    resp.setContentType("text/html");
         
-        PrintWriter out = response.getWriter();
+    PrintWriter out = resp.getWriter();
 
         
-        out.println("Test context attributes set by listener<br/>");
-        out.println("<br/>");
-        Dog dog = (Dog) getServletContext().getAttribute("dog");
-        out.println("Dog's breed is: " + dog.getBreed());
-        
-        out.flush();
-        out.close();
-    }
+    out.println("Test context attributes set by listener<br/>");
+    out.println("<br/>");
+    Dog dog = (Dog) getServletContext().getAttribute("dog");
+    out.println("Dog's breed is: " + dog.getBreed());
+       
+    out.flush();
+    out.close();
+  }
 }
