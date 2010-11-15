6e9a390a5b656645a72ea026182786801fb2d63d
henmar <henrik.martinussen@gmail.com>
2010-11-15 16:15:28 +0100

    comments and logging to console...

--- ch5-context-listener/src/main/java/info/martinussen/scwcd/hfsj/ch5/contexttest/Dog.java
+++ ch5-context-listener/src/main/java/info/martinussen/scwcd/hfsj/ch5/contexttest/Dog.java
@@ -3,8 +3,13 @@ package info.martinussen.scwcd.hfsj.ch5.contexttest;
 public class Dog {
 
   private String breed;
+  
+  static {
+    System.out.println("Dog class is loaded");
+  }
 
   public Dog(String Breed) {
+    System.out.println("Dog object is constructed, Breed is: " + breed);
     this.breed = Breed; 
   }
 
