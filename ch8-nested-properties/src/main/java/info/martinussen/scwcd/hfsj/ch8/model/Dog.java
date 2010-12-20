39b8f6273a83ef3a2cc26a75f61548d6674f45d8
henmar <henrik.martinussen@gmail.com>
2010-12-20 01:01:52 +0100

    forgot dog...

---
+++ ch8-nested-properties/src/main/java/info/martinussen/scwcd/hfsj/ch8/model/Dog.java
@@ -0,0 +1,14 @@
+package info.martinussen.scwcd.hfsj.ch8.model;
+
+public class Dog {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
