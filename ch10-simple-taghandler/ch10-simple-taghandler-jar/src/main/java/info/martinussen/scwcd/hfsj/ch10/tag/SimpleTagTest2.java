d991201ac997c388ba1b77a299d95d8ed53374c2
henmar <hma@mail.dk>
2011-01-07 16:49:53 +0100

    some further experiments with the ch10-simple-taghandler example...

--- ch10-simple-taghandler/ch10-simple-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/SimpleTagTest2.java
+++ ch10-simple-taghandler/ch10-simple-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/SimpleTagTest2.java
@@ -2,12 +2,42 @@ package info.martinussen.scwcd.hfsj.ch10.tag;
 
 import java.io.IOException;
 
+import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.JspTag;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+/**
+ * hfsj p 514
+ */
 public class SimpleTagTest2 extends SimpleTagSupport {
   
+  static {
+    System.out.println("Class SimpleTagTest2 loads");
+  }
+
+  public SimpleTagTest2(){
+    System.out.println("SimpleTagTest2 noArg constructor runs");
+  }
+  
+  public void setJspContext(JspContext pc) {
+    System.out.println("SimpleTagTest2.setJspContext(JspContext) runs");
+    super.setJspContext(pc);
+  }
+
+  public void setJspBody(JspFragment fragment){
+    System.out.println("SimpleTagTest2.setJspBody() runs");
+    super.setJspBody(fragment);
+  }
+
+  public void setParent(JspTag parent) {
+    System.out.println("SimpleTagTest1.setParent(JspTag) runs");
+    super.setParent(parent);
+  }
+
   public void doTag() throws JspException, IOException {
+    System.out.println("SimpleTagTest2.doTag() runs");
     getJspBody().invoke(null); //Process the body of the tag and print it to the response. The null argument means the output goes to the response rather than some OTHER writer you pass in. 
   }
 }
--- ch10-simple-taghandler/ch10-simple-taghandler-war/src/main/webapp/useTag.jsp
+++ ch10-simple-taghandler/ch10-simple-taghandler-war/src/main/webapp/useTag.jsp
@@ -3,6 +3,7 @@
 <html>
 <body>
 <h2>Simple Tag Handler Example Web Application</h2>
+
 Simple tag 1:<br/> 
 <myTags:simple1 user="${initParam.defaultUser}"/>
 <myTags:simple1 user="Rene'"></myTags:simple1><%-- whitespace is NOT allowed between the start and the end tag when tags bodycontent is supposed to be empty --%>
@@ -10,9 +11,11 @@ Simple tag 1:<br/>
 <myTags:simple1>
   <jsp:attribute name="user">Renè</jsp:attribute>
 </myTags:simple1> <%-- neither JSP comments nor html comments are allowed between the start and the end tag of the simple1 custom tag --%>
+
 Simple tag 2:<br/> 
 <myTags:simple2>And this go in the body ${initParam.defaultUser} ;-)</myTags:simple2><%-- EL is quite allright, scripting is disallowed though when tld says bodycontent is scriptless --%>
 <br/>
+<myTags:simple2>Another body content...</myTags:simple2> <%-- Doesn't work without body content... --%>
 
 </body>
 </html>
