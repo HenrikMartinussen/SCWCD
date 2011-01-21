006fc516f1557199262185bfc94a9efa3c4a2f90
henmar <hma@mail.dk>
2011-01-21 17:20:33 +0100

    introducing basic authetication

---
+++ ch12-basic-authentication/src/main/java/info/martinussen/scwcd/hfsj/ch12/UserServlet.java
@@ -0,0 +1,62 @@
+package info.martinussen.scwcd.hfsj.ch12;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * HFSJ p 663
+ * @author henmar
+ *
+ */
+@SuppressWarnings("serial")
+public class UserServlet extends HttpServlet{
+
+  static {
+    System.out.println("UserServlet class is loaded");
+  }
+
+  public UserServlet(){
+    super();
+    System.out.println("UserServlet is constructed");
+  }
+  
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException { 
+    
+    System.out.println("In UserServlet.doPost()");
+    RequestDispatcher view = req.getRequestDispatcher("WEB-INF/result.jsp");//removed a forward slash...
+    view.forward(req, resp);
+  }
+  
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("In UserServlet.doGet()");
+    doPost(req, resp);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    System.out.println("In UserServlet.service()");
+    super.service(req, resp);
+  }
+
+  @Override
+  public void init() throws ServletException {
+    System.out.println("In UserServlet.init()");
+    super.init();
+  }
+  
+  @Override
+  public void destroy(){
+    System.out.println("In UserServlet.destroy()");
+    super.destroy();
+  }
+}
---
+++ ch12-basic-authentication/src/main/webapp/WEB-INF/inputComments.jsp
@@ -0,0 +1,5 @@
+<form action="ProcessComments.do" method="post">
+  Add your comment: <br/>
+  <textarea rows="10" cols="40" name="newComment"></textarea><br/>
+  <input name="commentSubmit" type="submit" value="Add Comment" />
+</form>
