6d7f28147d432367da3a7c83e892c86d5f991f21
henmar <henrik.martinussen@gmail.com>
2011-02-17 17:38:35 +0100

    no dogs allowed in struts beerapp :-)

--- ch14-struts-beerapp/src/main/java/info/martinussen/scwcd/hfsj/ch14/listener/StartUpListener.java
+++ ch14-struts-beerapp/src/main/java/info/martinussen/scwcd/hfsj/ch14/listener/StartUpListener.java
@@ -1,8 +1,5 @@
 package info.martinussen.scwcd.hfsj.ch14.listener;
 
-
-import info.martinussen.scwcd.hfsj.ch14.model.Dog;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -13,10 +10,6 @@ public class StartUpListener implements ServletContextListener{
     System.out.println("in contextInitialized");
     ServletContext context = event.getServletContext();
     String breed = context.getInitParameter("Breed");
-    Dog dog = new Dog(breed);
-    context.setAttribute("Dog", dog);
-    
-    
   }
 
   public void contextDestroyed(ServletContextEvent event) {
