c51e55b59292d12c0f0176e7869ca7211b8b646d
henmar <hma@mail.dk>
2011-02-22 01:39:22 +0100

    still working on classic taghandler...

--- ch10-classic-taghandler/ch10-classic-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/ClassicTagTest4.java
+++ ch10-classic-taghandler/ch10-classic-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/ClassicTagTest4.java
@@ -6,26 +6,27 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
- * hfsj p 538
+ * hfsj p 538, 541
  * @author HMS
  *
  */
 public class ClassicTagTest4 extends TagSupport{
   
   private String[] movies = {"Spiderman", "Saved!", "Amelie"};
-  private int index = 0;
+  private int index; 
 
   
   public int doStartTag() throws JspException{
+    index = 0; // classic tags might be reused, so instance variables must be reset for each invocation
+    setMovie();
     return EVAL_BODY_INCLUDE;
   }
-  
+
   public int doAfterBody() throws JspException{
     
     int returnValue = SKIP_BODY;
     if (index < movies.length) {
-      pageContext.setAttribute("movie", movies[index]);
-      index++;
+      setMovie();
       returnValue = EVAL_BODY_AGAIN;
     } else {
       returnValue = SKIP_BODY;
@@ -36,5 +37,11 @@ public class ClassicTagTest4 extends TagSupport{
   public int doEndTag() throws JspException{
     return EVAL_PAGE;
   }
+
+  private void setMovie() {
+    pageContext.setAttribute("movie", movies[index]);
+    index++;
+  }
   
 }
+
