8f1f304d7520dd919ec1ddd31d226100a724a3c4
henmar <henrik.martinussen@gmail.com>
2011-02-23 11:51:57 +0100

    improved classic-tag-handler mockitotest improved

--- ch4/src/main/java/info/martinussen/scwcd/hfsj/ch4/BeerServlet.java
+++ ch4/src/main/java/info/martinussen/scwcd/hfsj/ch4/BeerServlet.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -47,14 +48,21 @@ public class BeerServlet extends HttpServlet{
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-
     System.out.println("service is called");
     super.service(req, resp);
   }
 
+  
+  
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    System.out.println("init(ServletConfig) is called");
+    super.init(config);
+  }
+
   @Override
   public void init() throws ServletException {
-    System.out.println("init is called");
+    System.out.println("init() is called");
     super.init();
   }
   
