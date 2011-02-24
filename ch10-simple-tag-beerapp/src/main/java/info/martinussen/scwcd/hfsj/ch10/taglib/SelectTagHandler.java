ff70688474c00b8bf4f7bb8c7f7005c07e7f21ef
henmar <henrik.martinussen@gmail.com>
2011-02-24 02:38:43 +0100

    ch10 simple tag handler beerapp finished

--- ch10-simple-tag-beerapp/src/main/java/info/martinussen/scwcd/hfsj/ch10/taglib/SelectTagHandler.java
+++ ch10-simple-tag-beerapp/src/main/java/info/martinussen/scwcd/hfsj/ch10/taglib/SelectTagHandler.java
@@ -1,40 +1,37 @@
 package info.martinussen.scwcd.hfsj.ch10.taglib;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.DynamicAttributes;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 /**
- * hfsj p 542
+ * hfsj p 542, 558
  * @author Henrik
  *
  */
-public class SelectTagHandler extends SimpleTagSupport {
+public class SelectTagHandler extends SimpleTagSupport implements DynamicAttributes {
   private static final String OPTION_START = "  <option value='";
   private static final String OPTION_END = " </option>";
-  private List optionsList;
-  private String name;
-  private String size;
+  private List<String> optionsList;
   
-  // store the 'optionsList' attribute
-  public void setOptionsList(List optionsList) {
+  private String name;
+  private Map<String, Object> tagAttrs = new HashMap<String, Object>();
+  
+  public void setOptionsList(List<String> optionsList) {
     this.optionsList = optionsList;
   }
   
-  // store the 'name' attribute
   public void setName(String name) {
     this.name = name;
   }
   
-  // store the 'size' attribute
-  public void setSize(String size) {
-    this.size = size;
-  }
-
   @Override
   // generate the <select> and <option> tags
   public void doTag() throws JspException, IOException {
@@ -42,18 +39,28 @@ public class SelectTagHandler extends SimpleTagSupport {
        JspWriter out = pageContext.getOut() ;
        String outString = "";
 
-       // Start the HTML <select> tag with HTML-specifc attributes
-       outString = "<select name='" + this.name + "' size='" + this.size + "'>";
+       // Start the HTML <select> tag with HTML-specific attributes
+       outString = "<select name='" + this.name + "'";
+       //add dynamic attributes
+       for (String attrName : tagAttrs.keySet()){
+         outString += " " + attrName + "='" + tagAttrs.get(attrName) + "'";
+       }
+       outString += ">";
        out.println(outString);
 
        // Generate the <option> tags from the optionsList
-       for (int i = 0; i < optionsList.size(); i++){
-         outString = OPTION_START + optionsList.get(i) + "'> " + optionsList.get(i) + OPTION_END;
+       for (String optionString : optionsList){
+         outString = OPTION_START + optionString + "'> " + optionString + OPTION_END;
          out.println(outString);
        }
        
        // End the HTML </select> tag
        outString = "</select>";
        out.println(outString);
+  }
+
+  public void setDynamicAttribute(String uri, String name, Object value)
+      throws JspException {
+    tagAttrs.put(name, value);
   }
 }
