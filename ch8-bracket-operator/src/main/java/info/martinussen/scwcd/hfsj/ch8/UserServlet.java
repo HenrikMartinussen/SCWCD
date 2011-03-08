a45ca2d1db8acd7382deae2aeb24ab43544153f9
henmar <henrik.martinussen@gmail.com>
2011-03-08 18:17:37 +0100

    ch8-bracket-operator created with examples concerning EL bracket operator

---
+++ ch8-bracket-operator/src/main/java/info/martinussen/scwcd/hfsj/ch8/UserServlet.java
@@ -0,0 +1,86 @@
+package info.martinussen.scwcd.hfsj.ch8;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * HFSJ p 376, 378
+ * @author henmar
+ *
+ */
+@SuppressWarnings("serial")
+public class UserServlet extends HttpServlet{
+  
+  static Logger log = Logger.getLogger(UserServlet.class);
+
+  static {
+    log.debug("UserServlet class is loaded");
+  }
+
+  public UserServlet(){
+    super();
+    log.debug("UserServlet is constructed");
+  }
+  
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException { 
+    
+    log.debug("doPost is called");
+    
+    Map<String, String> musicMap = new HashMap<String, String>();
+    musicMap.put("Ambient", "Zero 7");
+    musicMap.put("Surf", "Tahiti 80");
+    musicMap.put("DJ", "BT");
+    musicMap.put("Indie", "Nephew");
+    req.setAttribute("musicMap", musicMap);
+    
+    req.setAttribute("Genre", "Ambient");
+    
+    //http://www.exampledepot.com/egs/java.util/coll_GetArrayFromVector.html
+    String[] musicTypes = musicMap.keySet().toArray(new String[1]);
+    req.setAttribute("MusicType", musicTypes);
+    
+    
+    
+    
+    RequestDispatcher view = req.getRequestDispatcher("result.jsp");//removed a forward slash...
+    view.forward(req, resp);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    log.debug("service is called");
+    super.service(req, resp);
+  }
+
+  @Override
+  public void init() throws ServletException {
+    log.debug("init is called");
+    super.init();
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    log.debug("init(ServletConfig) is called");
+    super.init(config);
+  }
+
+  @Override
+  public void destroy(){
+    log.debug("destroy is called");
+    super.destroy();
+  }
+}
--- ch8-nested-properties/src/main/java/info/martinussen/scwcd/hfsj/ch8/model/Dog.java
+++ ch8-nested-properties/src/main/java/info/martinussen/scwcd/hfsj/ch8/model/Dog.java
@@ -5,11 +5,22 @@ import org.apache.log4j.Logger;
 public class Dog {
   private static Logger log = Logger.getLogger(Dog.class);
   private String name;
+  private String race;
   
   public Dog(){
     super();
     log.debug("Dog was constructed");
   }
+
+  public String getRace() {
+    return race;
+  }
+
+  public void setRace(String race) {
+    this.race = race;
+    log.debug("Dog.setRace was called, race parameter was " + race);
+  }
+
 
   public String getName() {
     return name;
@@ -19,5 +30,46 @@ public class Dog {
     this.name = name;
     log.debug("Dog.setName was called, name parameter was " + name);
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    log.debug("Dog.equals() was called");
+    boolean returnValue = false;
+    if (obj == this){
+      log.debug("obj and this was the same object");
+      returnValue = true;
+    } else{
+      if (obj != null){
+        log.debug("obj was not null");
+        if (obj instanceof Dog){
+          log.debug("obj was instance of Dog");
+          Dog other = (Dog) obj;
+          if (other.name != null){
+            log.debug("other.name was not null");
+            if (other.name.equalsIgnoreCase(this.name)){
+              log.debug("other.name was equal to this.name (ignoring case)");
+              if (other.race != null){
+                log.debug("other.race was not null");
+                if (other.race.equalsIgnoreCase(this.race)){
+                  log.debug("other.race was equal to this.race (ignoring case)");
+                  returnValue = true;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    log.debug("Dog.equals ends, " + returnValue + " is returned");
+    return returnValue;
+  }
+
+  @Override
+  public int hashCode() {
+    return 42; //usable but inefficient
+  }
+
+  
+  
 
 }
