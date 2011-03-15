95b7f650c115b8a5ca68210c16f38e640b583078
henmar <henrik.martinussen@gmail.com>
2011-03-15 01:01:15 +0100

    reporting filter exercise in core servlets and JavaServer Pages vol2 now works

---
+++ ch13-filters-ex/src/main/java/info/martinussen/scwcd/csjv2/ch5/ReportFilter.java
@@ -0,0 +1,52 @@
+package info.martinussen.scwcd.csjv2.ch5;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Core Servlets and JSP vol 2(csjv2), p 211
+ * @author HMS
+ *
+ */
+public class ReportFilter implements Filter{
+
+  private static Logger log = Logger.getLogger(ReportFilter.class);
+
+  static {
+    log.debug("ReportFilter is loaded");
+  }
+  
+  public ReportFilter(){
+    super();
+    log.debug("ReportFilter is constructed");
+  }
+  
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;//response is not cast - it isn't used
+    
+    log.info(req.getRemoteHost()     + 
+             " attempted to access " + 
+             req.getRequestURL()     + 
+             " on " + new Date());
+    filterChain.doFilter(req, response);    
+  }
+
+  public void init(FilterConfig config) throws ServletException {
+    log.debug("ReportFilter.init runs");//config is not saved - it isn't used
+  }
+
+  public void destroy() {
+    log.debug("ReportFilter.destroy runs");
+  }
+}
--- ch13-filters-ex/src/main/webapp/index.jsp
+++ ch13-filters-ex/src/main/webapp/index.jsp
@@ -1,5 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-  <body>
-    <h2>Hello Filter World!</h2>
-  </body>
+<head>
+<title>Filters 'R' Us</title>
+<link rel=STYLESHEET
+      href="styles/filter-styles.css"
+      type="text/css"/>
+</head>
+<body>
+<table border=5>
+  <tr><th class="TITLE">Filters 'R' Us</th></tr>
+</table>
+<table>
+  <tr>
+
+    <th><img src="images/air-filter.jpg" alt="Air Filter"/></th>
+    <th><img src="images/coffee-filter.gif" alt="Coffee Filter"/></th>
+    <th><img src="images/pump-filter.jpg" alt="Pump Filter"/></th>
+    </tr>
+</table>
+<h3>We specialize in the following:</h3>
+<ul>
+  <li>Air filters</li>
+  <li>Coffee filters</li>
+  <li>Pump filters</li>
+  <li>Camera lens filters</li>
+  <li>Image filters for Adobe Photoshop</li>
+  <li>Web content filters</li>
+  <li>Kalman filters</li>
+  <li>Servlet and JSP filters</li>
+
+</ul>
+<p>Check out <a href="TodaysSpecial">Today's Special</a>.</p>
+</body>
 </html>
