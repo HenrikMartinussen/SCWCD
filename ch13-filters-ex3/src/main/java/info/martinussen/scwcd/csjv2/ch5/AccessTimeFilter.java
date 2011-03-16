71e9771e69cbfc6742325843952f62949e600b4a
henmar <henrik.martinussen@gmail.com>
2011-03-16 18:00:35 +0100

    started an example from core servlets and jsp p 223 - unusual access times filter

---
+++ ch13-filters-ex3/src/main/java/info/martinussen/scwcd/csjv2/ch5/AccessTimeFilter.java
@@ -0,0 +1,78 @@
+package info.martinussen.scwcd.csjv2.ch5;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Core Servlets and JSP vol 2(csjv2), p 223
+ * Filter which logs access at a level which depends on the actual 
+ * time and the boundaries specified in web.xml
+ * @author HMS
+ *
+ */
+public class AccessTimeFilter implements Filter{
+
+  private static Logger log = Logger.getLogger(AccessTimeFilter.class);
+
+  static {
+    log.debug("LogFilter is loaded");
+  }
+
+  private FilterConfig filterConfig;
+  private ServletContext servletContext;
+  private String filterName;
+  private Date attentionTimeBegin;
+  
+  public AccessTimeFilter(){
+    super();
+    log.debug("LogFilter is constructed");
+  }
+  
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;//response is not cast - it isn't used
+    String logMessage = req.getRemoteHost()        + 
+                        " attempted to access "    + 
+                        req.getRequestURL()        + 
+                        " on " + new Date() + ". " + 
+                        "(Reported by " + filterName + ")";
+    
+    log.debug(logMessage);
+    servletContext.log(logMessage);
+    filterChain.doFilter(req, response);    
+  }
+
+  public void init(FilterConfig filterConfig) throws ServletException {
+    log.debug("LogFilter.init runs");
+    this.filterConfig = filterConfig;
+    servletContext = this.filterConfig.getServletContext();
+    filterName = this.filterConfig.getFilterName();
+    
+    String attentionTimeBeginString = this.filterConfig.getInitParameter("attentionTimeBegin");
+    DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+    try {
+      attentionTimeBegin = (Date)formatter.parse(attentionTimeBeginString);
+    } catch (ParseException e) {
+      log.error("Couldn't parse attentionTimeBegin filter init parameter", e);
+    }
+
+    log.info("attentionTimeBegin = " + attentionTimeBegin);
+  }
+
+  public void destroy() {
+    log.debug("LogFilter.destroy runs");
+  }
+}
---
+++ ch13-filters-ex3/src/main/java/info/martinussen/scwcd/csjv2/ch5/TodaysSpecialServlet.java
@@ -0,0 +1,56 @@
+package info.martinussen.scwcd.csjv2.ch5;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.apache.log4j.Logger;
+
+
+@SuppressWarnings("serial")
+public class TodaysSpecialServlet extends HttpServlet {
+  
+  private static Logger log = Logger.getLogger(TodaysSpecialServlet.class);
+  private String title;
+  private String picture;
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    log.debug("Simple Servlet doGet() runs");
+      
+    updateSpecials();
+    response.setContentType("text/html") ;
+    PrintWriter out = response.getWriter( ) ;
+    String docType =
+      " <! DOCTYPE HTML PUBLIC \" -/ /W3C//DTD HTML 4. 0 " +
+      " Transitional// EN\" >\n";
+    out.println
+      (docType +
+       "<HTML>\n" +
+       "<HEAD><TITLE>Today's Special</TITLE></HEAD>\n" +
+       "<BODY BGCOLOR=\"WHITE\">\n" +
+       "<CENTER>\n" +
+       "<H1>Today's Special: " + title + "s! </H1>\n" +
+       "<IMG SRC=\"images/" + picture + "\"\n" +
+       "     ALT=\"" + title + "\" >\n" +
+       "<BR CLEAR=\"ALL\" >\n" +
+       "Special deal: for only twice the price, you can\n" +
+       "<I>buy one, get one free! </I>.\n" +
+       "</BODY></HTML>");
+  }
+  
+  // Rotate among the three available filter images.
+  private void updateSpecials() {
+    double num = Math.random();
+    if (num < 0.333) {
+      title = "Air Filter" ;
+      picture = "air-filter.jpg" ;
+    } else if ( num < 0.666) {
+      title = "Coffee Filter";
+      picture = "coffee-filter.gif" ;
+    } else {
+      title = "Pump Filter" ;
+      picture = "pump-filter.jpg" ;
+    }
+  }
+}
