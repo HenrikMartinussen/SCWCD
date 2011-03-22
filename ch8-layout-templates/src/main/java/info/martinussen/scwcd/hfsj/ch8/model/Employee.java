9457fe8b42ec5ed2f57066a4c6e6b8c3857b5005
henmar <hma@mail.dk>
2011-03-23 00:47:10 +0100

    EL function experiments and include directive and jsp:include standard action exercises

---
+++ ch8-layout-templates/src/main/java/info/martinussen/scwcd/hfsj/ch8/model/Employee.java
@@ -0,0 +1,19 @@
+package info.martinussen.scwcd.hfsj.ch8.model;
+
+public class Employee extends Person {
+
+  private int empID;
+  
+  public Employee() {
+    super();
+  }
+
+  public int getEmpID() {
+    return empID;
+  }
+
+  public void setEmpID(int empID) {
+    this.empID = empID;
+  }
+
+}
---
+++ ch8-layout-templates/src/main/webapp/Header.jsp
@@ -0,0 +1,6 @@
+<%-- hfsj p 403 --%>
+
+<img src="images/Conformitech.jpg"><br/>
+
+<em><strong>${param.subTitle }</strong></em> <br/>
+
