51ad831f0dfcf15f76f67a59840e02552c017b8d
henmar <hma@mail.dk>
2011-03-29 19:12:15 +0200

    experimenting with form based authentication experimenting with simple taghandlers

--- ch10-simple-taghandler/ch10-simple-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/SimpleTagTest1.java
+++ ch10-simple-taghandler/ch10-simple-taghandler-jar/src/main/java/info/martinussen/scwcd/hfsj/ch10/tag/SimpleTagTest1.java
@@ -11,34 +11,37 @@ import javax.servlet.jsp.tagext.JspFragment;
 import javax.servlet.jsp.tagext.JspTag;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+import org.apache.log4j.Logger;
+
 public class SimpleTagTest1 extends SimpleTagSupport {
+  private static Logger log = Logger.getLogger(SimpleTagTest1.class);
   
   private String user;
 
   static {
-    System.out.println("Class SimpleTagTest1 loads");
+    log.debug("Class SimpleTagTest1 loads");
   }
   
   public SimpleTagTest1(){
-    System.out.println("SimpleTagTest1 noArg constructor runs");
+    log.debug("SimpleTagTest1 noArg constructor runs");
   }
   
   /**
    * This gives the handler a reference to a PageContext (a subclass of JspContext)
    */
   public void setJspContext(JspContext pc) {
-    System.out.println("SimpleTagTest1.setJspContext(JspContext) runs");
+    log.debug("SimpleTagTest1.setJspContext(JspContext) runs");
     super.setJspContext(pc);
   }
   
   /**
-   * This method is called if th tag i NOT declared to have a <body-content> 
+   * This method is called if the tag is NOT declared to have a <body-content> 
    * of empty, AND the tag has a body.<br/>
    * If the tag has a body, the body comes in through this method. as an 
    * instance of JspFragment.
    */
   public void setJspBody(JspFragment fragment){
-    System.out.println("SimpleTagTest1.setJspBody() runs");
+    log.debug("SimpleTagTest1.setJspBody() runs");
     super.setJspBody(fragment);
     
   }
@@ -47,7 +50,7 @@ public class SimpleTagTest1 extends SimpleTagSupport {
    * If the tag is nested (invoked from within another tag), this method is called. 
    */
   public void setParent(JspTag parent) {
-    System.out.println("SimpleTagTest1.setParent(JspTag) runs");
+    log.debug("SimpleTagTest1.setParent(JspTag) runs");
     super.setParent(parent);
   }
 
@@ -55,12 +58,12 @@ public class SimpleTagTest1 extends SimpleTagSupport {
    * Implementation of this tags behavior
    */
   public void doTag() throws JspException, IOException {
-    System.out.println("SimpleTagTest1.doTag() runs");
+    log.debug("SimpleTagTest1.doTag() runs");
     getJspContext().getOut().print(user + ": " + getStatement() + "<br/>");
   }
   
   public void setUser(String user){
-    System.out.println("SimpleTagTest1.setUser(String) runs - parameter user was " + user);
+    log.debug("SimpleTagTest1.setUser(String) runs - parameter user was " + user);
     this.user = user;
   }
   
