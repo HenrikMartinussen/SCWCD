7761721d4026203436fe046f50c2251bed001117
henmar <henrik.martinussen@gmail.com>
2012-01-05 16:34:15 +0100

    started groovy testing as a separate project... not working right now though

---
+++ ch4-groovy/src/main/java/info/martinussen/scwcd/hfsj/ch4/model/BeerExpert.java
@@ -0,0 +1,23 @@
+package info.martinussen.scwcd.hfsj.ch4.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BeerExpert {
+  
+  public List<String> getBrands(String color){
+    System.out.println("BeerExpert.getBrands() is called, parameter color was: " + color);
+    List<String> brands = new ArrayList<String>();
+    if (color.equals("amber")){
+      brands.add("Jack Amber");
+      brands.add("Red Moose");
+    } else {
+      brands.add("Jail Pale Ale");
+      brands.add("Gout Stout");
+    }
+    System.out.println("BeerExpert.getBrands() returns: " + brands);
+    return brands;
+  }
+
+}
