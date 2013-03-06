0c9496276ce9f2673c9d7769672e485a534d1ab5
Henrik Martinussen <henrik.martinussen@gmail.com>
2013-03-06 17:00:25 +0100

    try to fetch the data in a database

---
+++ ch13-filters-xsl/src/main/java/info/martinussen/scwcd/hfsj/ch13/model/XmlFileDataSource.java
@@ -0,0 +1,58 @@
+package info.martinussen.scwcd.hfsj.ch13.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.Logger;
+
+public class XmlFileDataSource implements XmlDataSource{
+  
+  private static final String RESOURCE = "/bookstore.xml";
+  private static Logger log = Logger.getLogger(XmlFileDataSource.class);
+  
+  
+  
+  static {
+    log.trace("XmlDataSource loaded");
+  }
+  
+  public XmlFileDataSource(){
+    log.trace("XmlDataSource constructed");
+  }
+  
+  public String getXmlData(){
+    String returnValue = null;
+    
+    InputStream in  = this.getClass().getResourceAsStream(RESOURCE);
+    if (in != null){
+      log.debug("resource retrieved successfully");
+    } else {
+      log.error("error retrieving resource");
+    }
+    
+    try {
+      returnValue =  streamToString(in);
+    } catch (IOException e) {
+      log.warn("XmlDataSource.getXmlData(): An IOException was caught, null will be returned - " + e);
+    }
+    return returnValue;
+  }
+
+  private String streamToString(InputStream inputStream) throws IOException {
+    StringBuilder builder = new StringBuilder();
+    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+    
+
+    String line = br.readLine();
+    while (line != null) {
+      builder.append(line);
+      line = br.readLine();
+    }
+    br.close();
+    log.debug(builder.toString());
+    return builder.toString();
+  }
+
+}
