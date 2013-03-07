770de1f30943a26ad11ce7c132990e27f008d72f
Henrik Martinussen <henrik.martinussen@gmail.com>
2013-03-07 09:57:32 +0100

    prepared the startuplistener to be configured to use alternative XmlDataSource implementations - e.g. one that fetches xml in the database...

--- ch13-filters-xsl/src/main/java/info/martinussen/scwcd/hfsj/ch13/StartupListener.java
+++ ch13-filters-xsl/src/main/java/info/martinussen/scwcd/hfsj/ch13/StartupListener.java
@@ -4,6 +4,7 @@ package info.martinussen.scwcd.hfsj.ch13;
 import info.martinussen.scwcd.hfsj.ch13.model.XmlDataSource;
 import info.martinussen.scwcd.hfsj.ch13.model.XmlFileDataSource;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -12,8 +13,10 @@ import org.apache.log4j.Logger;
 public class StartupListener implements ServletContextListener {
 
 	private static Logger log = Logger.getLogger(StartupListener.class);
+	private static final String ATTRIBUTE_NAME = "xmlDataSource";
 	
-	private XmlDataSource xmlDataSource;
+	private ServletContext servletContext = null;
+	private XmlDataSource xmlDataSource = null;
 
 	static {
 		log.trace("StartupListener loaded");
@@ -26,12 +29,23 @@ public class StartupListener implements ServletContextListener {
 	}
 	
 	public void contextInitialized(ServletContextEvent event) {
-		log.debug("Context initialized");
-		xmlDataSource = new XmlFileDataSource();
-		event.getServletContext().setAttribute("xmlDataSource", xmlDataSource);
+		log.debug("StartupListener.contextInitialized() is called");
+		servletContext = event.getServletContext();
+		String dataSourceClassName = (String) servletContext.getInitParameter("xmlDataSource");
+		try {
+      xmlDataSource = (XmlDataSource) Class.forName(dataSourceClassName).newInstance();
+    } catch (Exception e) {
+      String message = "Error instantiating Class " + dataSourceClassName + " by reflection";
+      log.fatal(message + " " + e);
+      throw new IllegalStateException(e);
+    } 
+		
+		event.getServletContext().setAttribute(ATTRIBUTE_NAME, xmlDataSource);
+		log.debug("xmlDataSource was added to ServletContext under the name " + ATTRIBUTE_NAME);
 	}
 
 	public void contextDestroyed(ServletContextEvent event) {
+	  servletContext.removeAttribute(ATTRIBUTE_NAME);
 		log.debug("Context destroyed");
 	}
 }
