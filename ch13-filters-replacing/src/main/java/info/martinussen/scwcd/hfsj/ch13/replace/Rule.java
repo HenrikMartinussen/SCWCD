bf4e0c3c3fe5e567c43d46b80be50a1a7f19604d
Henrik Martinussen <hma@mail.dk>
2013-03-27 17:07:01 +0100

    made the property file approach work...

--- ch13-filters-replacing/src/main/java/info/martinussen/scwcd/hfsj/ch13/replace/Rule.java
+++ ch13-filters-replacing/src/main/java/info/martinussen/scwcd/hfsj/ch13/replace/Rule.java
@@ -1,16 +1,15 @@
 package info.martinussen.scwcd.hfsj.ch13.replace;
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 
 public class Rule {
-  private Logger log = Logger.getLogger(Rule.class);
+  //private Logger log = Logger.getLogger(Rule.class);
   private String toBeReplaced;
   private String replaceWith;
   
   public Rule(String tbr, String rw){
     this.toBeReplaced = tbr;
     this.replaceWith = rw;
-    log.debug("Rule constructed - tbr: " + tbr + " rw: " + rw);
   }
 
   public String getToBeReplaced() {
@@ -20,6 +19,14 @@ public class Rule {
   public String getReplaceWith() {
     return replaceWith;
   }
+  
+  public String toString(){
+    return "Rule[Replace " + toBeReplaced  + " with " + replaceWith + "]";
+  }
+  
+  //Maybe rules could be chained, passing the String to be changed along the chain
+  //Thus delegating the responsibility to do the change to the rule object - According to 
+  //the oop principle "Don't ask an object for data so you can do the work, ask the object to do the work"
 }
   
   
