c1c1c0dd4e872917ec4cbafec51c29d427362f3d
Henrik Martinussen <henrik.martinussen@gmail.com>
2013-07-05 14:13:01 +0200

    added logging and input filtering

--- ch4-download/src/main/java/info/martinussen/keyfile/KeyFileServlet.java
+++ ch4-download/src/main/java/info/martinussen/keyfile/KeyFileServlet.java
@@ -3,12 +3,10 @@ package info.martinussen.keyfile;
 import info.martinussen.keyfile.model.ProductKey;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -40,14 +38,18 @@ public class KeyFileServlet extends HttpServlet{
     log.debug("KeyFileServlet.doPost XmlKey: \r\n" + xmlKey);
     
     resp.setContentType("text/xml");
+    resp.setContentLength(xmlKey.length());
     resp.setHeader("Content-Disposition",
-                       "attachment;filename=" + buildProposedFileName(req)); 
+                       " :attachment; filename=\"" + buildProposedFileName(req) + "\""); 
 
-    OutputStream os = resp.getOutputStream();
+    ServletOutputStream os = resp.getOutputStream();
     byte[] outBytes = xmlKey.getBytes();
     os.write(outBytes);
+    log.trace("os.write finished");
     os.flush();
+    log.trace("os.flush finished");
     os.close();
+    log.trace("os.close finished");
   }
   
   private String buildProposedFileName(HttpServletRequest req){
@@ -58,8 +60,10 @@ public class KeyFileServlet extends HttpServlet{
     ProductKey key = new ProductKey();
     
     String version = req.getParameter("version"); assertNotNull(version);
-    String customer = req.getParameter("customerName"); assertNotNull(customer);//todo filter contents
-    String orderNumber = req.getParameter("orderNumber"); assertNotNull(orderNumber);//todo filter contents
+    String customerRaw = req.getParameter("customerName"); assertNotNull(customerRaw);
+    String customer = filterNonXmlChars(customerRaw);
+    String orderNumberRaw = req.getParameter("orderNumber"); assertNotNull(orderNumberRaw); 
+    String orderNumber =  numericFilter(orderNumberRaw); 
     String clientCountStr = req.getParameter("clientCount"); assertNotNull(clientCountStr);
     int clientCount = Integer.parseInt(clientCountStr);
     String automatCountStr = req.getParameter("automatCount"); assertNotNull(automatCountStr);
@@ -94,6 +98,7 @@ public class KeyFileServlet extends HttpServlet{
     return key;
   }
   
+
   private boolean isCheckBoxChecked(HttpServletRequest req, String checkBoxName){
     String checkBoxString = req.getParameter(checkBoxName);
     //unchecked checkboxes is not included in the requestParameter List
@@ -102,6 +107,86 @@ public class KeyFileServlet extends HttpServlet{
   
   private void assertNotNull(Object o){
     if (o == null) {throw new IllegalStateException("Unexpected null value");}
+  }
+
+  /** Replaces characters that have special HTML meanings
+   *  with their corresponding HTML character entities.
+   */
+  /*  Note that Javadoc is not used for the more detailed
+      documentation due to the difficulty of making the
+      special chars readable in both plain text and HTML.
+  
+      Given a string, this method replaces all occurrences of
+      '<' with '&lt;', all occurrences of '>' with
+      '&gt;', and (to handle cases that occur inside attribute
+      values), all occurrences of double quotes with
+      '&quot;' and all occurrences of '&' with '&amp;'.
+      Without such filtering, an arbitrary string
+      could not safely be inserted in a Web page.
+    */
+  public static String filterNonXmlChars(String input) {
+    if (!hasNonXmlChars(input)) {
+      return(input);
+    }
+    StringBuffer filtered = new StringBuffer(input.length());
+    char c;
+    for(int i=0; i<input.length(); i++) {
+      c = input.charAt(i);
+      switch(c) {
+        case '<': filtered.append("&lt;"); break;
+        case '>': filtered.append("&gt;"); break;
+        case '"': filtered.append("&quot;"); break;
+        case '&': filtered.append("&amp;"); break;
+//        case 'æ': filtered.append("&aelig;"); break;
+//        case 'Æ': filtered.append("&Aelig;"); break;
+//        case 'ø': filtered.append("&oslash;"); break;
+//        case 'Ø': filtered.append("&Oslash;"); break;
+//        case 'å': filtered.append("&aring;"); break;
+//        case 'Å': filtered.append("&Aring;"); break;
+        default: filtered.append(c);
+      }
+    }
+    if (input.length() != filtered.length()){
+      log.info("KeyFileServlet.filter changed " + input + " into " + filtered.toString());
+    }
+    return(filtered.toString());
+  }
+  
+  private static boolean hasNonXmlChars(String input) {
+    boolean hasSpecialChars = false;
+    if ((input != null) && (input.length() > 0)) {
+      char c;
+      for(int i=0; i<input.length(); i++) {
+        c = input.charAt(i);
+        switch(c) {
+//          case 'æ': hasSpecialChars = true; break;
+//          case 'Æ': hasSpecialChars = true; break;
+//          case 'ø': hasSpecialChars = true; break;
+//          case 'Ø': hasSpecialChars = true; break;
+//          case 'å': hasSpecialChars = true; break;
+//          case 'Å': hasSpecialChars = true; break;
+          case '&': hasSpecialChars = true; break;
+          case '<': hasSpecialChars = true; break;
+          case '>': hasSpecialChars = true; break;
+          case '"': hasSpecialChars = true; break;
+        }
+      }
+    }
+    return(hasSpecialChars);
+  }
+
+  private String numericFilter(String numberString) {
+    StringBuilder builder = new StringBuilder();
+    char[] chars = numberString.toCharArray();
+    for (char c : chars){
+      if (c >= '0' && c <= '9' ) {
+        builder.append(c);
+      }
+    }
+    if (numberString.length() != builder.length()){
+      log.info("KeyFileServlet.numericFilter changed " + numberString + " into " + builder.toString());
+    }
+    return builder.toString();
   }
 
   @Override
