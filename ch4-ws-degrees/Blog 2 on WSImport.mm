75bbf3cd937ae1e335f819c73a4a33bfacee63c9
henmar <henrik.martinussen@gmail.com>
2013-12-29 23:59:14 +0100

    documentation

---
+++ ch4-ws-degrees/Blog 2 on WSImport.mm
@@ -0,0 +1,49 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1388331833439" ID="Freemind_Link_616001851" MODIFIED="1388357554827" TEXT="Blog #2&#xa;on WSImport">
+<node CREATED="1388331847459" ID="_" MODIFIED="1388331875315" POSITION="right" TEXT="Links">
+<node CREATED="1388331930664" ID="Freemind_Link_1671674581" MODIFIED="1388331937520" TEXT="Javaranch"/>
+<node CREATED="1388331938181" ID="Freemind_Link_1876272302" MODIFIED="1388331951083" TEXT="Ken Kousen"/>
+<node CREATED="1388331955794" ID="Freemind_Link_262320473" MODIFIED="1388331960064" TEXT="Roger Goosen"/>
+<node CREATED="1388331967673" ID="Freemind_Link_1714172230" MODIFIED="1388331973531" TEXT="Gradle in action"/>
+<node CREATED="1388331974161" ID="Freemind_Link_101869679" MODIFIED="1388331986381" TEXT="previous blog"/>
+<node CREATED="1388357320499" ID="Freemind_Link_757525144" MODIFIED="1388357328526" TEXT="MKYoung"/>
+</node>
+<node CREATED="1388331876426" ID="Freemind_Link_1064538182" MODIFIED="1388331890043" POSITION="right" TEXT="Findings">
+<node CREATED="1388331891330" ID="Freemind_Link_889009740" MODIFIED="1388331900437" TEXT="Testreport"/>
+<node CREATED="1388331900895" ID="Freemind_Link_440955666" MODIFIED="1388331907774" TEXT="Gradle version"/>
+</node>
+<node CREATED="1388331920962" ID="Freemind_Link_530555132" MODIFIED="1388331928646" POSITION="left" TEXT="Technologies">
+<node CREATED="1388332051591" ID="Freemind_Link_1418352239" MODIFIED="1388332055314" TEXT="Gradle">
+<node CREATED="1388357355967" ID="Freemind_Link_135554125" MODIFIED="1388357377578" TEXT="Multi module Gradle build"/>
+</node>
+<node CREATED="1388332056428" ID="Freemind_Link_26463603" MODIFIED="1388332061299" TEXT="Eclipse"/>
+<node CREATED="1388332061897" ID="Freemind_Link_743856220" MODIFIED="1388332079259" TEXT="WSImport"/>
+<node CREATED="1388332083934" ID="Freemind_Link_1965258951" MODIFIED="1388332086220" TEXT="Java"/>
+<node CREATED="1388332171530" ID="Freemind_Link_1404998452" MODIFIED="1388332183887" TEXT="Parameterized JUnit test">
+<node CREATED="1388332184522" ID="Freemind_Link_1488014082" MODIFIED="1388332199437" TEXT="Unit test"/>
+<node CREATED="1388332204347" ID="Freemind_Link_346845607" MODIFIED="1388332215584" TEXT="Integrationtest"/>
+</node>
+</node>
+<node CREATED="1388331991979" ID="Freemind_Link_1938864206" MODIFIED="1388331995793" POSITION="left" TEXT="Purpose">
+<node CREATED="1388331997560" ID="Freemind_Link_875526053" MODIFIED="1388332049600" TEXT="Using gradle as buildtool, while generating Java classes from a wsdl using WSImport"/>
+</node>
+<node CREATED="1388332114260" ID="Freemind_Link_1838737117" MODIFIED="1388332116494" POSITION="right" TEXT="Case">
+<node CREATED="1388332117638" ID="Freemind_Link_1813792496" MODIFIED="1388332152596" TEXT="converting between degrees celsius and degrees Fahrenheit">
+<node CREATED="1388332153957" ID="Freemind_Link_70913482" MODIFIED="1388332160079" TEXT="and vice versa"/>
+</node>
+</node>
+<node CREATED="1388357392047" ID="Freemind_Link_184195721" MODIFIED="1388357407853" POSITION="left" TEXT="To Be improved">
+<node CREATED="1388357408608" ID="Freemind_Link_1756321901" MODIFIED="1388357434872" TEXT="Dry">
+<node CREATED="1388357460732" ID="Freemind_Link_1269328213" MODIFIED="1388357477632" TEXT="two almost identical testcases"/>
+</node>
+<node CREATED="1388357441443" ID="Freemind_Link_724114034" MODIFIED="1388357447377" TEXT="Canonicality"/>
+<node CREATED="1388357637852" ID="Freemind_Link_520156686" MODIFIED="1388357666703" TEXT="unit- and integrationtests in separate folder structures"/>
+<node CREATED="1388357722687" ID="Freemind_Link_1488413273" MODIFIED="1388357727486" TEXT="warnings"/>
+<node CREATED="1388357728163" ID="Freemind_Link_651643313" MODIFIED="1388357832688" TEXT="logging lags similarity from class to class"/>
+</node>
+<node CREATED="1388357578241" ID="Freemind_Link_66863818" MODIFIED="1388357587289" POSITION="right" TEXT="Method">
+<node CREATED="1388357588356" ID="Freemind_Link_1292746419" MODIFIED="1388357608779" TEXT="as described on Javaranch"/>
+</node>
+</node>
+</map>
