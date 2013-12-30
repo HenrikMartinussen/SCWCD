10643b439058a9166f0e0efb0a9ddd4c5c7a8eea
Henrik Martinussen <henrik.martinussen@gmail.com>
2013-12-30 16:24:13 +0100

    documentation

--- ch4-ws-degrees/Blog 2 on WSImport.mm
+++ ch4-ws-degrees/Blog 2 on WSImport.mm
@@ -1,49 +1,154 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1388331833439" ID="Freemind_Link_616001851" MODIFIED="1388357554827" TEXT="Blog #2&#xa;on WSImport">
-<node CREATED="1388331847459" ID="_" MODIFIED="1388331875315" POSITION="right" TEXT="Links">
-<node CREATED="1388331930664" ID="Freemind_Link_1671674581" MODIFIED="1388331937520" TEXT="Javaranch"/>
-<node CREATED="1388331938181" ID="Freemind_Link_1876272302" MODIFIED="1388331951083" TEXT="Ken Kousen"/>
-<node CREATED="1388331955794" ID="Freemind_Link_262320473" MODIFIED="1388331960064" TEXT="Roger Goosen"/>
-<node CREATED="1388331967673" ID="Freemind_Link_1714172230" MODIFIED="1388331973531" TEXT="Gradle in action"/>
-<node CREATED="1388331974161" ID="Freemind_Link_101869679" MODIFIED="1388331986381" TEXT="previous blog"/>
-<node CREATED="1388357320499" ID="Freemind_Link_757525144" MODIFIED="1388357328526" TEXT="MKYoung"/>
+<node CREATED="1388394857357" ID="Freemind_Link_1689386813" MODIFIED="1388395028206" POSITION="left" TEXT="introduction">
+<node CREATED="1388394862749" ID="Freemind_Link_1548165727" MODIFIED="1388410946816" TEXT="summary">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1388405566722" ID="Freemind_Link_952852723" MODIFIED="1388406355798" TEXT="Technical blog, describing how to make a java webservice, with a WSDL as the starting point (&quot;Contract First&quot;). JAX-WS WSImport is used to generate java classes corresponding to the WSDL. WSImport is executed using its ant task from a gradle build file. "/>
 </node>
-<node CREATED="1388331876426" ID="Freemind_Link_1064538182" MODIFIED="1388331890043" POSITION="right" TEXT="Findings">
-<node CREATED="1388331891330" ID="Freemind_Link_889009740" MODIFIED="1388331900437" TEXT="Testreport"/>
-<node CREATED="1388331900895" ID="Freemind_Link_440955666" MODIFIED="1388331907774" TEXT="Gradle version"/>
-</node>
-<node CREATED="1388331920962" ID="Freemind_Link_530555132" MODIFIED="1388331928646" POSITION="left" TEXT="Technologies">
-<node CREATED="1388332051591" ID="Freemind_Link_1418352239" MODIFIED="1388332055314" TEXT="Gradle">
+<node CREATED="1388395031722" ID="Freemind_Link_655494416" MODIFIED="1388410949218" TEXT="Technologies used">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1388332051591" ID="Freemind_Link_1418352239" MODIFIED="1388395373275" TEXT="Gradle 1.10">
 <node CREATED="1388357355967" ID="Freemind_Link_135554125" MODIFIED="1388357377578" TEXT="Multi module Gradle build"/>
 </node>
 <node CREATED="1388332056428" ID="Freemind_Link_26463603" MODIFIED="1388332061299" TEXT="Eclipse"/>
 <node CREATED="1388332061897" ID="Freemind_Link_743856220" MODIFIED="1388332079259" TEXT="WSImport"/>
-<node CREATED="1388332083934" ID="Freemind_Link_1965258951" MODIFIED="1388332086220" TEXT="Java"/>
+<node CREATED="1388332083934" ID="Freemind_Link_1965258951" MODIFIED="1388406422548" TEXT="Java Servlet API 2.5"/>
+<node CREATED="1388394527985" ID="Freemind_Link_453881229" MODIFIED="1388394537477" TEXT="SoapUi"/>
 <node CREATED="1388332171530" ID="Freemind_Link_1404998452" MODIFIED="1388332183887" TEXT="Parameterized JUnit test">
 <node CREATED="1388332184522" ID="Freemind_Link_1488014082" MODIFIED="1388332199437" TEXT="Unit test"/>
 <node CREATED="1388332204347" ID="Freemind_Link_346845607" MODIFIED="1388332215584" TEXT="Integrationtest"/>
 </node>
 </node>
+<node CREATED="1388395044580" ID="Freemind_Link_1451362188" MODIFIED="1388410951709" TEXT="flow catalysts">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1388399543929" ID="Freemind_Link_620280302" MODIFIED="1388399559717" TEXT="Blog by Ken Kousen"/>
+<node CREATED="1388399561408" ID="Freemind_Link_148082096" MODIFIED="1388399599102" TEXT="chapters in the upcoming book &quot;Gradle in Action&quot; by Benjamin Muschko">
+<node CREATED="1388399600122" ID="Freemind_Link_1806186757" MODIFIED="1388399611158" TEXT="on gradle multiprojects"/>
+<node CREATED="1388399611546" ID="Freemind_Link_201097254" MODIFIED="1388399622999" TEXT="on gradle integration test"/>
+</node>
+</node>
+<node CREATED="1388395107581" ID="Freemind_Link_1220036907" MODIFIED="1388395111409" TEXT="references">
+<node CREATED="1388331930664" ID="Freemind_Link_1671674581" MODIFIED="1388399512748" TEXT="Coderanch">
+<node CREATED="1388395586039" ID="Freemind_Link_1632070137" MODIFIED="1388395599595" TEXT="Monica. Shiralkar"/>
+<node CREATED="1388395574047" ID="Freemind_Link_274855971" MODIFIED="1388395584483" TEXT="Abhay Agarwal"/>
+<node CREATED="1388399072830" ID="Freemind_Link_1530732266" MODIFIED="1388399074449" TEXT="http://www.coderanch.com/t/625506/Web-Services/java/searching-simple-practising-creation-Contract"/>
+</node>
+<node CREATED="1388331955794" ID="Freemind_Link_262320473" MODIFIED="1388331960064" TEXT="Roger Goosen">
+<node CREATED="1388399128525" ID="Freemind_Link_503355863" MODIFIED="1388399130064" TEXT="http://rphgoossens.wordpress.com/2011/02/20/developing-a-contract-first-jax-ws-webservice/"/>
+</node>
+<node CREATED="1388331974161" ID="Freemind_Link_101869679" MODIFIED="1388331986381" TEXT="previous blog">
+<node CREATED="1388399274228" ID="Freemind_Link_764800838" MODIFIED="1388399275495" TEXT="http://springundertheskin.wordpress.com/2013/06/"/>
+</node>
+<node CREATED="1388331938181" ID="Freemind_Link_1876272302" MODIFIED="1388331951083" TEXT="Ken Kousen">
+<node CREATED="1388399209480" ID="Freemind_Link_1778680216" MODIFIED="1388399210812" TEXT="http://jaxenter.com/tutorial-gradle-soap-features-revealed-42757.html"/>
+</node>
+<node CREATED="1388331967673" ID="Freemind_Link_1714172230" MODIFIED="1388331973531" TEXT="Gradle in action">
+<node CREATED="1388399406987" ID="Freemind_Link_1498441723" MODIFIED="1388399415208" TEXT="Benjamin Muschko"/>
+<node CREATED="1388399417788" ID="Freemind_Link_494049865" MODIFIED="1388399420551" TEXT="http://www.manning.com/muschko/"/>
+</node>
+<node CREATED="1388357320499" ID="Freemind_Link_757525144" MODIFIED="1388357328526" TEXT="MKYoung">
+<node CREATED="1388399361638" ID="Freemind_Link_162042379" MODIFIED="1388399363082" TEXT="http://www.mkyong.com/webservices/jax-ws/jax-ws-hello-world-example/"/>
+</node>
+</node>
+<node CREATED="1388406456374" ID="Freemind_Link_1900286120" MODIFIED="1388406459794" TEXT="links">
+<node CREATED="1388405422092" ID="Freemind_Link_414886222" MODIFIED="1388405426912" TEXT="bitbucket">
+<node CREATED="1388406502068" ID="Freemind_Link_1364857410" MODIFIED="1388406503391" TEXT="https://bitbucket.org/henmar/scwcd/src/1faf745ae951/ch4-ws-degrees/?at=default"/>
+</node>
+</node>
+<node CREATED="1388397647539" ID="Freemind_Link_1822021077" MODIFIED="1388406620961" TEXT="I am in the process of migrating from maven to gradle in my toy projects and technology demonstrator projects, following an initiative in my workplace to gradually replace a batch/ant build file complex."/>
+<node CREATED="1388397857851" ID="Freemind_Link_1434957424" MODIFIED="1388397921936" TEXT="Gradle has been elected as strategic build technology at my workplace, after evaluating maven 3 and gradle"/>
+</node>
 <node CREATED="1388331991979" ID="Freemind_Link_1938864206" MODIFIED="1388331995793" POSITION="left" TEXT="Purpose">
 <node CREATED="1388331997560" ID="Freemind_Link_875526053" MODIFIED="1388332049600" TEXT="Using gradle as buildtool, while generating Java classes from a wsdl using WSImport"/>
+<node CREATED="1388395323806" ID="Freemind_Link_729545623" MODIFIED="1388395357658" TEXT="Migrate from maven 3 to gradle"/>
 </node>
-<node CREATED="1388332114260" ID="Freemind_Link_1838737117" MODIFIED="1388332116494" POSITION="right" TEXT="Case">
+<node CREATED="1388357578241" ID="Freemind_Link_66863818" MODIFIED="1388357587289" POSITION="left" TEXT="Method">
+<node CREATED="1388357588356" ID="Freemind_Link_1292746419" MODIFIED="1388399504035" TEXT="as described by Abhay Agawal on Coderanch"/>
+</node>
+<node CREATED="1388394474983" ID="Freemind_Link_897506485" MODIFIED="1388394484483" POSITION="left" TEXT="The bullet list">
 <node CREATED="1388332117638" ID="Freemind_Link_1813792496" MODIFIED="1388332152596" TEXT="converting between degrees celsius and degrees Fahrenheit">
 <node CREATED="1388332153957" ID="Freemind_Link_70913482" MODIFIED="1388332160079" TEXT="and vice versa"/>
 </node>
 </node>
-<node CREATED="1388357392047" ID="Freemind_Link_184195721" MODIFIED="1388357407853" POSITION="left" TEXT="To Be improved">
+<node CREATED="1388394487327" ID="Freemind_Link_1485713152" MODIFIED="1388394509812" POSITION="left" TEXT="The corresponding artifacts">
+<node CREATED="1388399665461" ID="Freemind_Link_312818363" MODIFIED="1388399669040" TEXT="Schemas"/>
+<node CREATED="1388399669428" ID="Freemind_Link_1066571020" MODIFIED="1388399672616" TEXT="WSDL"/>
+<node CREATED="1388399677069" ID="Freemind_Link_112175728" MODIFIED="1388399683417" TEXT="Generated java classes"/>
+<node CREATED="1388399687693" ID="Freemind_Link_1822243962" MODIFIED="1388407572127" TEXT="Rudimentary algorithm implementation, and corresponding parameterized unit test">
+<node CREATED="1388407996091" ID="Freemind_Link_14791750" MODIFIED="1388408116235" TEXT="e.g. It converts to and from a temperature of -274 degrees perfectly well - even though it is below absolute zero"/>
+</node>
+</node>
+<node CREATED="1388394232439" ID="Freemind_Link_1734292327" MODIFIED="1388394241266" POSITION="left" TEXT="build/test">
+<node CREATED="1388394242455" ID="Freemind_Link_1980225740" MODIFIED="1388394251930" TEXT="gradle clean build"/>
+<node CREATED="1388394259071" ID="Freemind_Link_1937463788" MODIFIED="1388394264172" TEXT="test report">
+<node CREATED="1388394265400" ID="Freemind_Link_89115930" MODIFIED="1388394267283" TEXT="degrees-ws-war\build\reports\allTests\index.html"/>
+</node>
+<node CREATED="1388394570010" ID="Freemind_Link_1116207560" MODIFIED="1388394575134" TEXT="run service">
+<node CREATED="1388394592162" ID="Freemind_Link_1799100001" MODIFIED="1388394658337" TEXT="gradle jettyRun"/>
+<node CREATED="1388394663510" ID="Freemind_Link_643241500" MODIFIED="1388394667442" TEXT="browse to ">
+<node CREATED="1388394705895" ID="Freemind_Link_204131617" MODIFIED="1388394706627" TEXT="http://localhost:8081/degrees-ws-war/degreesService?wsdl"/>
+<node CREATED="1388394724696" ID="Freemind_Link_1883728726" MODIFIED="1388394726148" TEXT="http://localhost:8081/degrees-ws-war/degreesService"/>
+</node>
+</node>
+<node CREATED="1388394737104" ID="Freemind_Link_622287246" MODIFIED="1388394753333" TEXT="start new SoapUi project">
+<node CREATED="1388394754497" ID="Freemind_Link_309492983" MODIFIED="1388394776934" TEXT="point soapUi new project wizard to the wsdl url"/>
+</node>
+<node CREATED="1388394780834" ID="Freemind_Link_125578395" MODIFIED="1388394807599" TEXT="create automatic soapui system tests"/>
+</node>
+<node CREATED="1388357392047" ID="Freemind_Link_184195721" MODIFIED="1388395180924" POSITION="right" TEXT="improvement potential">
 <node CREATED="1388357408608" ID="Freemind_Link_1756321901" MODIFIED="1388357434872" TEXT="Dry">
 <node CREATED="1388357460732" ID="Freemind_Link_1269328213" MODIFIED="1388357477632" TEXT="two almost identical testcases"/>
 </node>
-<node CREATED="1388357441443" ID="Freemind_Link_724114034" MODIFIED="1388357447377" TEXT="Canonicality"/>
-<node CREATED="1388357637852" ID="Freemind_Link_520156686" MODIFIED="1388357666703" TEXT="unit- and integrationtests in separate folder structures"/>
-<node CREATED="1388357722687" ID="Freemind_Link_1488413273" MODIFIED="1388357727486" TEXT="warnings"/>
-<node CREATED="1388357728163" ID="Freemind_Link_651643313" MODIFIED="1388357832688" TEXT="logging lags similarity from class to class"/>
+<node CREATED="1388394292481" ID="Freemind_Link_1600426083" MODIFIED="1388394306773" TEXT="make the jar build more generic"/>
+<node CREATED="1388357637852" ID="Freemind_Link_520156686" MODIFIED="1388357666703" TEXT="unit- and integrationtests in separate folder structures">
+<node CREATED="1388394434182" ID="Freemind_Link_852562414" MODIFIED="1388394452201" TEXT="eliminates some of the test report issues"/>
 </node>
-<node CREATED="1388357578241" ID="Freemind_Link_66863818" MODIFIED="1388357587289" POSITION="right" TEXT="Method">
-<node CREATED="1388357588356" ID="Freemind_Link_1292746419" MODIFIED="1388357608779" TEXT="as described on Javaranch"/>
+<node CREATED="1388357722687" ID="Freemind_Link_1488413273" MODIFIED="1388357727486" TEXT="warnings"/>
+<node CREATED="1388357728163" ID="Freemind_Link_651643313" MODIFIED="1388406765962" TEXT="logging lacks similarity from class to class"/>
+</node>
+<node CREATED="1388394334122" ID="Freemind_Link_426689818" MODIFIED="1388394346326" POSITION="right" TEXT="ideas for future projects">
+<node CREATED="1388394348378" ID="Freemind_Link_282581671" MODIFIED="1388394352255" TEXT="groovy test"/>
+<node CREATED="1388394353059" ID="Freemind_Link_572544144" MODIFIED="1388394357167" TEXT="groovy client"/>
+</node>
+<node CREATED="1388395219873" ID="Freemind_Link_475436595" MODIFIED="1388395224758" POSITION="right" TEXT="conclusion">
+<node CREATED="1388395855553" ID="Freemind_Link_1186972738" MODIFIED="1388406859548" TEXT="Once I let maven go, it is easy to use groovy as test implementation language"/>
+<node CREATED="1388395894770" ID="Freemind_Link_631214369" MODIFIED="1388395949575" TEXT="...and it is easy to separate the unit test and the integration test into separate folderstructures"/>
+<node CREATED="1388331876426" ID="Freemind_Link_1064538182" MODIFIED="1388331890043" TEXT="Findings">
+<node CREATED="1388331891330" ID="Freemind_Link_889009740" MODIFIED="1388331900437" TEXT="Testreport"/>
+<node CREATED="1388331900895" ID="Freemind_Link_440955666" MODIFIED="1388331907774" TEXT="Gradle version">
+<node CREATED="1388393657770" ID="Freemind_Link_1291834145" MODIFIED="1388393661597" TEXT="errormessage">
+<node CREATED="1388393662858" ID="Freemind_Link_1265233990" MODIFIED="1388393664270" TEXT="Could not find property &apos;reports&apos; on task &apos;:degrees-ws-war:test&apos;.">
+<node CREATED="1388393671722" ID="Freemind_Link_420071178" MODIFIED="1388393677910" TEXT="@gradle 1.5"/>
+<node CREATED="1388393678802" ID="Freemind_Link_1265313907" MODIFIED="1388393686728" TEXT="@gradle 1.6"/>
+</node>
+</node>
+<node CREATED="1388393741988" ID="Freemind_Link_1529354126" MODIFIED="1388393754579" TEXT="version 1.10 solves it"/>
+</node>
+</node>
+<node CREATED="1388396224022" ID="_" MODIFIED="1388396251882" TEXT="pros and cons of maven3 and gradle as I experience them so far">
+<node CREATED="1388396255055" ID="Freemind_Link_412755107" MODIFIED="1388399029031" TEXT="My experience level in maven 3 and gradle differ."/>
+<node CREATED="1388396283000" ID="Freemind_Link_1263985190" MODIFIED="1388396376527" TEXT="gradle api&apos;s is undergoing significant change from one minor version to the next"/>
+<node CREATED="1388396381275" ID="Freemind_Link_153019429" MODIFIED="1388396464394" TEXT="mvn convention and implementation makes it hard to use otherwise reasonable ideas">
+<node CREATED="1388396465406" ID="Freemind_Link_1015909070" MODIFIED="1388396517004" TEXT="separate unit test and integrations test into different folder structures"/>
+<node CREATED="1388396517560" ID="Freemind_Link_1993377803" MODIFIED="1388396556269" TEXT="implement tests using groovy"/>
+</node>
+<node CREATED="1388396631516" ID="Freemind_Link_1046074181" MODIFIED="1388396675634" TEXT="gradle lacks the &quot;good gut feeling&quot; of maven">
+<node CREATED="1388396676190" ID="Freemind_Link_1353692563" MODIFIED="1388397540306" TEXT="If it works in maven, I&apos;m walking on mine-free territory"/>
+<node CREATED="1388396745000" ID="Freemind_Link_1835892264" MODIFIED="1388396916250" TEXT="even if I can make it work with gradle, it doesn&apos;t necessarily mean that I&apos;m doing it &quot;right&quot;"/>
+<node CREATED="1388397241672" ID="Freemind_Link_1240558491" MODIFIED="1388397481488" TEXT="I fear ending up in ant-hell, where a buildfile complex can be unmaintainable for new project members, or even for yourself - if you have been busy doing other projects for a couple of months."/>
+</node>
+<node CREATED="1388398084859" ID="Freemind_Link_778683994" MODIFIED="1388398084862" TEXT="several of the tasks that were hard in maven is pleasantly easy in gradle &#xa;">
+<node CREATED="1388398095291" ID="Freemind_Link_1515330808" MODIFIED="1388398121231" TEXT="mixing java and groovy"/>
+</node>
+<node CREATED="1388397942492" ID="Freemind_Link_1704315207" MODIFIED="1388398286405" TEXT="maven, being as mature as it is, has articles with examples and &quot;headaches solved&quot; in plenty on the net"/>
+<node CREATED="1388398323611" ID="Freemind_Link_378262798" MODIFIED="1388399011768" TEXT="The developers of gradle doesn&apos;t seem to fully appreciate the qualities of maven, discarding simple conventions such as the results of a build being stored in the build folder as opposed to the target folder and letting the .gradle folder survive a gradle clean, and letting the .settings folder survive a gradle cleanEclipse. Of course you can customize your build file to behave like maven in these respects, but it adds &quot;mud&quot; to the build files - exactly the opposite to what you want to achieve when migrating from maven to gradle."/>
+</node>
+<node CREATED="1388396917318" ID="Freemind_Link_1132704750" MODIFIED="1388397052355" TEXT="maybe you should aim for your gradle build files to be short, conventional and immediately readable, in order to keep the good gut feeling."/>
+<node CREATED="1388397055179" ID="Freemind_Link_1766374790" MODIFIED="1388397184327" TEXT="gradle build file hacking is for people who perceive maven&apos;s structure and convention features as being a hindrance rather than a help"/>
+<node CREATED="1388397562616" ID="Freemind_Link_327197571" MODIFIED="1388406866142" TEXT="for the rest of us, we get to enjoy white-space instead of angled brackets in our build files"/>
+<node CREATED="1388407087290" ID="Freemind_Link_1045109482" MODIFIED="1388407476880" TEXT="I warmly recommend the upcoming book &quot;Gradle in Action&quot; by Benjamin Muschko, it was a great help in creating the gradle build files used in this example. You can get access to a preliminary version under Manning&apos;s Early Access Program (MEAP)"/>
+<node CREATED="1388406869042" ID="Freemind_Link_87466394" MODIFIED="1388407060501" TEXT="Thanks to Roger Goosen, Ken Kousen, MKYoung for sharing their knowledge and experience. Thanks to Abhay Agarwal for a constructive and pedagogical answer to Monica. Shiralkars Coderanch question."/>
 </node>
 </node>
 </map>
