8c167df97f48be1e1f4f43e4f4f8669cf12c320d
henmar <hma@mail.dk>
2014-02-08 01:20:54 +0100

    small steps...

--- ch4-groovy2/src/main/webapp/WEB-INF/groovy/hello.groovy
+++ ch4-groovy2/src/main/webapp/WEB-INF/groovy/hello.groovy
@@ -1,2 +1,6 @@
+import info.martinussen.scwcd.hfsj.ch4.model.BeerExpert
+
+BeerExpert expert = context.getAt("beerExpert")
+assert expert != null
 name = params.name ?: 'World'
 println "Hello, $name!"
