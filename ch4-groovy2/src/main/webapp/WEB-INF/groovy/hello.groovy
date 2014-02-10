946e5650aba9460d55e877789ac47faac80a343e
Henrik Martinussen <henrik.martinussen@gmail.com>
2014-02-10 10:02:50 +0100

    added an index.html made the groovlet return html (to be remade later using a builder)

--- ch4-groovy2/src/main/webapp/WEB-INF/groovy/hello.groovy
+++ ch4-groovy2/src/main/webapp/WEB-INF/groovy/hello.groovy
@@ -2,5 +2,17 @@ import info.martinussen.scwcd.hfsj.ch4.model.BeerExpert
 
 BeerExpert expert = context.getAt("beerExpert")
 assert expert != null
-name = params.name ?: 'World'
-println "Hello, $name!"
+color = params.color ?: 'amber'
+def brands = []
+brands =  expert.getBrands(color)
+assert brands.size() > 0
+println '<html>'
+println '<body>'
+println '<h1 align="center">Beer recommendations</h1>'
+println '<p>'
+brands.each{
+  println "<br/>Try: ${it}"
+}
+println '</p>'
+println '</body>'
+println '</html>'
