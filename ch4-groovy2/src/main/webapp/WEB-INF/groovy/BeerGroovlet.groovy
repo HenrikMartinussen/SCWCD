843860b2ac5098f5c93f8a8ba53b275db80f91d8
Henrik Martinussen <hma@mail.dk>
2014-02-10 17:54:28 +0100

    replaced printlined html with built html

--- ch4-groovy2/src/main/webapp/WEB-INF/groovy/BeerGroovlet.groovy
+++ ch4-groovy2/src/main/webapp/WEB-INF/groovy/BeerGroovlet.groovy
@@ -6,13 +6,16 @@ color = params.color ?: 'amber'
 def brands = []
 brands =  expert.getBrands(color)
 assert brands.size() > 0
-println '<html>'
-println '<body>'
-println '<h1 align="center">Beer recommendations</h1>'
-println '<p>'
-brands.each{
-  println "<br/>Try: ${it}"
+
+html.html{
+  head{
+    title 'Beer Recommendations'
+  }
+  body {
+    h1 'Beer Recommendations'
+    brands.each{
+      p "Try: ${it}"
+    }
+  }
 }
-println '</p>'
-println '</body>'
-println '</html>'
+
