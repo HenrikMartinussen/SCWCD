e9bd2f164578c9e40397fd1c46892315a2c404f6
Henrik Martinussen <hma@mail.dk>
2014-10-22 20:28:20 +0200

    work in progress - I'm on to something...

--- ch4-groovy2/src/test/groovy/info/martinussen/scwcd/hfsj/ch4/model/TestBeerExpert.groovy
+++ ch4-groovy2/src/test/groovy/info/martinussen/scwcd/hfsj/ch4/model/TestBeerExpert.groovy
@@ -1,45 +1,28 @@
 package info.martinussen.scwcd.hfsj.ch4.model
 
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertTrue
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
-import org.apache.log4j.Logger
-import info.martinussen.scwcd.hfsj.ch4.model.BeerExpert
+import spock.lang.Specification
+import spock.lang.Unroll
 
-@RunWith(Parameterized)
-public class TestBeerExpert{
-
-  Logger log = Logger.getLogger(TestBeerExpert.class)
-
+class TestBeerExpert4 extends Specification{
+  
   BeerExpert testExpert = null;
-  def param
-  def expected1
-  def expected2
+  
+  def setup() { testExpert = new BeerExpert() }
+  
+  def cleanup() { testExpert = null}
 
-  @Parameters
-  static data () {
-    return [["amber", "Jack Amber",    "Red Moose"]  as String[],
-            ["light", "Jail Pale Ale", "Gout Stout"] as String[],
-            ["brown", "Jail Pale Ale", "Gout Stout"] as String[],
-            ["dark",  "Jail Pale Ale", "Gout Stout"] as String[]]
+  
+  @Unroll
+  def "#color should result in #advice"() {
+  
+  expect: testExpert.getBrands(color) == advice
+  
+  where:
+  color   | advice
+  'amber' | ['Jack Amber', 'Red Moose']
+  'light' | ['Jail Pale Ale', 'Gout Stout']
+  'brown' | ['Jail Pale Ale', 'Gout Stout']
+  'dark'  | ['Jail Pale Ale', 'Gout Stout']
   }
-
-  TestBeerExpert (p , ex1, ex2){
-    param = p
-    expected1 = ex1
-    expected2 = ex2
-  }
-
-  @Test
-  void testGetBrands(){
-    testExpert = new BeerExpert()
-    def result = testExpert.getBrands(param)
-    assertTrue result.size() == 2
-    assertTrue result.contains(expected1)
-    assertTrue result.contains(expected2)
-    testExpert = null;
-  }
+  
 }
