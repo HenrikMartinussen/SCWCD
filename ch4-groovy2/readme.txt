f1144261c463b5fa47d63fdd157ab1924fb5c839
Henrik Martinussen <henrik.martinussen@gmail.com>
2014-10-23 18:13:44 +0200

    HttpBuilder/xmlSlurper integration test works documentation, readme updated

--- ch4-groovy2/readme.txt
+++ ch4-groovy2/readme.txt
@@ -8,18 +8,20 @@ The baseline is the Beer recommendation webapp in chapter 4 of head First Servle
 This project is a groovy/gradle rework of the ch4 demo project
 
 Demonstrates 
--implementing a mvc model by means of a groovy class
--simple parameterized spock test of the groovy model implemented in groovy
--implementing a servlet api startup listener in groovy
--Groovlet accessing request and context implicit variables, building html
--Parameterized HtmlUnit integration test implemented in groovy
--Gradle integration test task, which starts Jetty and subsequently stops it again
+- implementing a mvc model by means of a groovy class
+- simple parameterized spock test of the groovy model implemented in groovy
+- implementing a servlet api startup listener in groovy
+- Groovlet accessing request and context implicit variables, building html
+- Parameterized HtmlUnit integration test implemented in groovy
+- Parameterized integration test using HTTPBuilder and xmlSlurper supplied GPath
+- Gradle integration test task, which starts Jetty, runs integration tests 
+  recognized by Classname convention), and subsequently stops jetty again
 
 In order to run test:
 gradle clean test
 
 In order to run web app: 
-gradle clean build jettyRun
+gradle clean jettyRun
 
 browse to http://localhost:8081/ch4-groovy2/index.html
 or curl -i --data "color=light" http://localhost:8081/ch4-groovy2/BeerGroovlet.groovy
--- ch4-groovy2/src/test/groovy/info/martinussen/scwcd/hfsj/ch4/integration/BeerGroovlet2IntegrationTest.groovy
+++ ch4-groovy2/src/test/groovy/info/martinussen/scwcd/hfsj/ch4/integration/BeerGroovlet2IntegrationTest.groovy
@@ -1,37 +1,65 @@
 package info.martinussen.scwcd.hfsj.ch4.integration
 
-import static groovyx.net.http.ContentType.URLENC
-import org.junit.After;
-import org.junit.Before;
+import static groovyx.net.http.ContentType.HTML
+import static org.junit.Assert.fail
+
+import groovy.util.slurpersupport.GPathResult
+import groovyx.net.http.HTTPBuilder
 import org.junit.Test;
-import org.apache.log4j.Logger
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized)
 class BeerGroovlet2IntegrationTest {
-  
-  def  log = Logger.getLogger(BeerGroovlet2IntegrationTest.class)
-  def port = '8081'
-  def url = "http://localhost:${port}/ch4-groovy2/BeerGroovlet.groovy"
-  def httpBuilder 
-  
+/* Integrationtest demonstrating HTTPBuilder executing POST requests  and
+ * XML slurper making the html in the response available with GPath expressions.
+ * Futhermore this integrationtest demonstrates JUnit 4 parameterized test, 
+ * implemented in groovy.
+ * 
+ * The test simulates a push on the submit button on the index.html page, and
+ * verifies that the resulting page has the expected contents, corresponding 
+ * to the request parameters. 
+ *
+ * This test doesn't verify the contents of - or navigation from the index.html page
+ */
+ 
+  def port    = '8081'
+  def url     = "http://localhost:${port}"
+  def path    = "/ch4-groovy2/BeerGroovlet.groovy"
+  def HTTP_OK = 200
+  def http 
 
-  @After
-  void tearDown(){
-    
+  def param
+  def expected1
+  def expected2
+  
+  @Parameters
+  static data () {
+    return [["amber", "Jack Amber",    "Red Moose"]  as String[],
+            ["light", "Jail Pale Ale", "Gout Stout"] as String[],
+            ["brown", "Jail Pale Ale", "Gout Stout"] as String[],
+            ["dark",  "Jail Pale Ale", "Gout Stout"] as String[]]
   }
   
-  
+  BeerGroovlet2IntegrationTest (p , ex1, ex2){
+    param = p
+    expected1 = ex1
+    expected2 = ex2
+  }
+
   @Test
-  public void testAmber(){
-    httpBuilder = new HTTPBuilder(url)
-    def postBody = [color: 'light']
-    http.post(path: url, body:postBody, requestContenttype: URLENC){ resp ->
-      assert resp.statusLine.statusCode == 200
+  public void testAllColors(){
+    http = new HTTPBuilder(url)
+    def (html, responseStatus) = http.post(
+                                    body:[color: param], 
+                                    path:path, 
+                                    contentType:HTML){ resp, reader ->
+      [reader, resp.status]
     }
-    
-    log.debug(resp)
-    
+    assert responseStatus == HTTP_OK
+    assert html instanceof GPathResult
+    assert html.BODY.P.text() =~ "Try: $expected1"   
+    assert html.BODY.P.text() =~ "Try: $expected2"   
   }
-  
-
 }
