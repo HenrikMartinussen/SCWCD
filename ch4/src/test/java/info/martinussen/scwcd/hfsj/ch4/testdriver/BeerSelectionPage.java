9c0995a41fb2f2997d27d7ed594718abe10feded
Henrik Martinussen <henrik.martinussen@gmail.dk>
2016-02-09 01:08:35 +0100

    beginning a test framework

---
+++ ch4/src/test/java/info/martinussen/scwcd/hfsj/ch4/testdriver/BeerSelectionPage.java
@@ -0,0 +1,10 @@
+package info.martinussen.scwcd.hfsj.ch4.testdriver;
+
+public class BeerSelectionPage {
+    public static final String COLOR_LIGHT = "light";
+    public static final String COLOR_AMBER = "amber";
+    public static final String COLOR_BROWN = "brown";
+    public static final String COLOR_DARK  = "dark";
+
+
+}
