23eaf7d2223f8b9bcc111d32607765bcf0db3dd1
Henrik Martinussen <henrik.martinussen@gmail.com>
2016-02-17 00:28:10 +0100

    cxf example started

---
+++ ch4-ws-cxf/src/main/java/info/martinussen/beerexpert/service/BeerExpertServiceImpl.java
@@ -0,0 +1,64 @@
+package info.martinussen.beerexpert.service;
+
+import info.martinussen.beerexpert.model.BeerExpert;
+import info.martinussen.beerexpert.service.generated.BeerExpertServicePortType;
+import info.martinussen.beerexpert.service.generated.BeerExpertServiceRequestType;
+import info.martinussen.beerexpert.service.generated.BeerExpertServiceResponseType;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+
+import org.apache.log4j.Logger;
+
+//http://rphgoossens.wordpress.com/2011/02/20/developing-a-contract-first-jax-ws-webservice/
+
+@WebService(endpointInterface="info.martinussen.beerexpert.service.generated.BeerExpertServicePortType")
+public class BeerExpertServiceImpl implements BeerExpertServicePortType {
+  
+  static Logger log = Logger.getLogger(BeerExpertServiceImpl.class);
+  
+  static {
+    log.trace("BeerExpertServiceImpl was loaded");
+  }
+
+  @Resource
+  WebServiceContext webServiceContext;
+
+  public BeerExpertServiceImpl(){
+    super();
+    log.trace("BeerExpertServiceImpl was instantiated");
+  }
+  
+  @Override
+  @WebMethod(action = "http://martinussen.info/BeerAdviseService/getBeerAdvise")
+  @WebResult(name = "BeerExpertServiceResponse", 
+             targetNamespace = "http://martinussen.info/beerexpertservice/1.0", 
+             partName = "BeerExpertServiceResponse")
+  public BeerExpertServiceResponseType getBeerAdvise( 
+              @WebParam(name = "BeerExpertServiceRequest", 
+                        targetNamespace = "http://martinussen.info/beerexpertservice/1.0", 
+                        partName = "BeerExpertServiceRequest") 
+              BeerExpertServiceRequestType beerExpertServiceRequest) {
+
+    log.debug("BeerExpertServiceImpl.getBeerAdvise() was called");
+    log.debug("BeerExpertServiceImpl.getBeerAdvise(); webServiceContext=" + webServiceContext);
+    String color = beerExpertServiceRequest.getColor();
+    BeerExpert beerExpert = new BeerExpert();
+    List<String> advice = beerExpert.getBrands(color);
+    log.debug("BeerExpertServiceImpl called beerExpert with parameter " + color);
+    log.debug("beerExpert responded: " + advice.toString());
+    BeerExpertServiceResponseType response = new BeerExpertServiceResponseType();
+    for (String element : advice){
+      response.getAdvice().add(element);
+    }
+    log.debug("BeerExpertServiceImpl.getBeerAdvise returns...");
+    return response;
+  }
+
+}
---
+++ ch4-ws-cxf/src/test/java/info/martinussen/beerexpert/service/BeerExpertServiceClient.java
@@ -0,0 +1,42 @@
+package info.martinussen.beerexpert.service;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import info.martinussen.beerexpert.service.generated.*;
+
+/**
+ * Test client 
+ * http://www.mkyong.com/webservices/jax-ws/jax-ws-hello-world-example/
+ */
+public class BeerExpertServiceClient {
+  
+  public static final String WSDL_URL = "http://localhost:8081/beerexpert-ws/beerExpertService?wsdl";
+  public static final String SERVICE_URI = "http://service.beerexpert.martinussen.info/";
+  public static final String SERVICE_NAME = "BeerExpertServiceImplService";
+  
+  BeerExpertServicePortType port = null;
+  
+  public BeerExpertServiceClient(){
+    URL url = null;
+    try {
+      url = new URL(WSDL_URL);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e); // rethrow as rte
+    }
+    QName qname = new QName(SERVICE_URI, SERVICE_NAME);
+    Service service = Service.create(url, qname);
+    port = service.getPort(BeerExpertServicePortType.class);
+  }
+      
+  public List<String> getBrands(String color){
+    BeerExpertServiceRequestType request = new BeerExpertServiceRequestType();
+    request.setColor(color);
+    BeerExpertServiceResponseType response = port.getBeerAdvise(request);
+    return response.getAdvice();
+  }
+}
